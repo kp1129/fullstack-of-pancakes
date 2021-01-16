fc70547e825b601e129da926eb4674832b9e040a
kp1129 <katya.p.1129@gmail.com>
2021-01-15 23:43:10 -0700

    add localStorage hook

---
+++ app/src/useLocalStorage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+        // check if key exists in local storage
+        // if not, set value to initialValue
+        let item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+
+    // custom updater function to update value 
+    // (associated with this key) in app and localStorage
+    const setStoredValue = (mode) => {
+        setValue(mode);
+        window.localStorage.setItem(key, JSON.stringify(mode))
+    }
+
+    return [value, setStoredValue];
+}
