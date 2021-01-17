fd1c2f6ac492abcb5fa6c2b26303167af8bf4f94
kp1129 <katya.p.1129@gmail.com>
2021-01-16 21:22:47 -0700

    fix bug, remove localstorage, simplify toggle into header component

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   BrowserRouter as Router,
@@ -26,10 +26,29 @@ import "./App.css";
 import "animate.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const styleBody = document.body.style;
+    const links = document.getElementsByTagName("a");
+    if (darkMode) {
+      styleBody.backgroundColor = "#000";
+      styleBody.color = "#fff";
+      for (let i = 0; i < links.length; i++){
+        links[i].style.color = "#fff";
+      }
+    } else {
+      styleBody.backgroundColor = "#fff";
+      styleBody.color = "#000";
+      for (let i = 0; i < links.length; i++){
+        links[i].style.color = "#000";
+      }
+    }
+  }, [darkMode]);
   return (
     <Router>
       <div className="app">
-        <Header />
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
         <Switch>
           <Route path="/about">
             <About />
--- app/src/Header.js
+++ app/src/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
-import ModeToggle from './ModeToggle';
+import { NavLink } from "react-router-dom";
 
-function Header() {
+function Header({ darkMode, setDarkMode }) {
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  };
   return (
     <div className="header">
-      <div className="theme-toggle">
-          {/* <ModeToggle /> */}
+      {/* dark mode toggle */}
+      <div
+        onClick={handleClick}
+        className={darkMode ? "theme-toggle toggled" : "theme-toggle"}
+      >
+        <div className="toggle"></div>
       </div>
+      {/* navigation */}
       <div className="navbar">
         <NavLink exact to="/">
           HOME
@@ -15,7 +22,13 @@ function Header() {
         <NavLink to="/about">ABOUT</NavLink>
         <NavLink to="/projects">PROJECTS</NavLink>
         <NavLink to="/connect">CONNECT</NavLink>
-        <a href="https://my.searchlight.ai/katya.p.1129" target="_blank">REFERENCES</a>
+        <a
+          href="https://my.searchlight.ai/katya.p.1129"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          REFERENCES
+        </a>
       </div>
     </div>
   );
