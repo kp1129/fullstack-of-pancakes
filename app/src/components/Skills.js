396dd74482a7ca1722378c246867ebe1f83ec493
kp1129 <katya.p.1129@gmail.com>
2021-01-19 22:44:28 -0700

    add more media queries

--- app/src/components/Skills.js
+++ app/src/components/Skills.js
@@ -1,27 +1,16 @@
 import React from "react";
-import '../css/Skills.css';
+import "../css/Skills.css";
 
 function Skills() {
   return (
     <div className="skills">
       <p>
-        {" "}
-        My favorite part of the stack is frontend (
-        <span role="img" aria-label="sparkles">
-          ✨
-        </span>
-        React, Redux, CSS, LESS/SASS, Bootstrap, Material UI, styled-components,
-        etc) but I'm just as comfortable diving into backend (
-        <span role="img" aria-label="sparkles">
-          ✨
-        </span>
-        Node, Python), deploying (
-        <span role="img" aria-label="sparkles">
-          ✨
-        </span>
-        Heroku, Vercel, Netlify), and learning new tech on the go. Check out my projects on Github!
+        My favorite part of the stack is frontend (React, Redux, CSS,
+        LESS/SASS, Bootstrap, Material UI, styled-components, etc) but I'm just
+        as comfortable diving into backend (Node, Python), deploying (Heroku,
+        Vercel, Netlify), and learning new tech on the go. Check out my projects
+        on Github!
       </p>
-
     </div>
   );
 }
